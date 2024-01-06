a193188be7316118e7f42a46933b5e5dfaa8e4ba
writtenbyrla <133620285+writtenbyrla@users.noreply.github.com>
2024-01-06 12:10:21 +0900

    Time: 49 ms (93.12%), Space: 17.5 MB (5.76%) - LeetHub

--- 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.py
+++ 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.py
@@ -1,13 +1,5 @@
-import heapq
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
         
-        # 최소힙으로 변환되는 것이 기본이므로 최대힙으로 heapify를 해야함
-        nums = [-num for num in nums]
-        heapq.heapify(nums)
-        
-        num1 = -heapq.heappop(nums)
-        num2 = -heapq.heappop(nums)
-        
-        return (num1-1)*(num2-1)
-        
+        nums.sort(reverse=True)
+        return (nums[0] -1) * (nums[1] - 1)
