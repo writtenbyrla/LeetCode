8fc4cd87ec9c372439257bd6ff3fd7273c6c04a9
writtenbyrla <133620285+writtenbyrla@users.noreply.github.com>
2024-01-06 14:30:42 +0900

    Time: 100 ms (77.36%), Space: 17.6 MB (22.79%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.py
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.py
@@ -2,20 +2,15 @@ import heapq
 class Solution:
     def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
         
-        # 리스트에 1이 나온 개수를 카운트해서 순서대로 담음
-        min_heap = [(row.count(1), i) for i, row in enumerate(mat)]
-            
-        # 리스트 -> 힙
-        heapq.heapify(min_heap)
+        min_heap = []
         
-        # 답을 반환할 리스트 선언
+        for i in range(len(mat)):
+            cnt = collections.Counter(mat[i])
+            heapq.heappush(min_heap, (cnt[1], i))
+        
         answer = []
 
         for i in range(k):
-            count, index = heapq.heappop(min_heap)
-            answer.append(index)
+            answer.append(heapq.heappop(min_heap)[1])
 
         return answer
-            
-        
-        
