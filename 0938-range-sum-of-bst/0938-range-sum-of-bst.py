8c8a179ed655d07262179250a8b3f928acadc502
writtenbyrla <133620285+writtenbyrla@users.noreply.github.com>
2024-01-12 18:01:59 +0900

    Time: 123 ms (62.33%), Space: 24.3 MB (99.28%) - LeetHub

--- 0938-range-sum-of-bst/0938-range-sum-of-bst.py
+++ 0938-range-sum-of-bst/0938-range-sum-of-bst.py
@@ -7,7 +7,7 @@
 class Solution:
     def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
         
-#         low~ high 범위에 있는 수 더하기
+        # low~ high 범위에 있는 수 더하기
         
         # 노드 없으면 0 반환
         if not root:
@@ -16,16 +16,31 @@ class Solution:
         sum = 0
         stack = [root]
 
-        # dfs 방식(stack 이용)
+        # 1. dfs 방식(stack 이용) - 트리
+        # while stack:
+        #     node = stack.pop()
+        #     if node:
+        #         # 범위안에 있으면 해당 값 더하고
+        #         if low <= node.val <= high:
+        #             sum += node.val
+        #
+        #         # 서브 트리 계속 순회함
+        #         stack.append(node.left)
+        #         stack.append(node.right)
+
+        #bst = 완전 이진 트리이므로 서브트리의 왼쪽은 값이 작고 오른쪽은 값이 크다
+        # 2. 현재 노드 크기를 low, high 비교해서 오른쪽으로 갈지 왼쪽으로 갈지 결정
+        
         while stack:
             node = stack.pop()
             if node:
-                # 범위안에 있으면 해당 값 더하고
+                if node.val < low:
+                    stack.append(node.right)
+                if node.val > high:
+                    stack.append(node.left)
                 if low <= node.val <= high:
                     sum += node.val
-                    
-                #서브 트리 계속 순회함
-                stack.append(node.left)
-                stack.append(node.right)
+                    stack.append(node.left)
+                    stack.append(node.right)
 
         return sum
