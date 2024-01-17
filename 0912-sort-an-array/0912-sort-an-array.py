ab452face1ec0ca1e1c309979b7be47e1991a4d3
writtenbyrla <133620285+writtenbyrla@users.noreply.github.com>
2024-01-17 11:46:02 +0900

    Time: 1114 ms (74.30%), Space: 26.3 MB (26.98%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.py
+++ 0912-sort-an-array/0912-sort-an-array.py
@@ -1,3 +1,35 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
-        return sorted(nums)
+        
+        def merge(arr1, arr2):
+            result = []
+            i = j = 0
+            while i < len(arr1) and j < len(arr2):
+                if arr1[i] < arr2[j]:
+                    result.append(arr1[i])
+                    i += 1
+                else:
+                    result.append(arr2[j])
+                    j += 1
+
+            while i < len(arr1):
+                result.append(arr1[i])
+                i += 1
+
+            while j < len(arr2):
+                result.append(arr2[j])
+                j += 1
+
+            return result
+
+        def mergesort(nums):
+            if len(nums) <= 1:
+                return nums
+
+            mid = len(nums) // 2
+            L = nums[:mid]
+            R = nums[mid:]
+
+            return merge(mergesort(L), mergesort(R))
+
+        return mergesort(nums)
