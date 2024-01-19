12ddc5fb576313b8a9aa01a4dd9396824a6e68fa
writtenbyrla <133620285+writtenbyrla@users.noreply.github.com>
2024-01-19 10:16:48 +0900

    Time: 42 ms (83.13%), Space: 17.6 MB (49.91%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -17,19 +17,20 @@ class Solution:
         if not nums:
             return -1  
         
+        # 최솟값 찾기(left)
         left = 0
         right = len(nums)-1
         while left < right:
             mid = (left + right) // 2
-            if nums[mid] > nums[right]:
-                left = mid + 1
+            if nums[mid] > nums[right]: # 중간지점의 값이 더 크면
+                left = mid + 1  # 오른쪽으로 더 이동해서 찾아야함
             else:
-                right = mid
+                right = mid # 중간지점의 값이 더 작으면 왼쪽으로 이동해서 찾아야함
         
-        added = nums + nums[:left]
+        added = nums + nums[:left] # 최솟값 기준으로 뒤에 덧붙임
         result = bs(added, left, len(added)-1)
         
         if result == -1:
             return result
         else:
-            return result % len(nums)
+            return result % len(nums) # 원래 인덱스 찾아주기
