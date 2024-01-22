f3f7f67a49cc44e58207033bb11e903e5a83ec4c
writtenbyrla <133620285+writtenbyrla@users.noreply.github.com>
2024-01-22 13:41:05 +0900

    Time: 575 ms (56.78%), Space: 30.9 MB (59.60%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,9 +1,9 @@
 from typing import List
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        result = []
-        result.append(nums[0])
-        for i in range(1, len(nums)):
-            # 현재까지의 합과 현재값을 비교해서 더 큰 경우를 result에 담음
-            result.append(max(result[i-1] + nums[i], nums[i]))
-        return max(result) 
+        max_sum = nums[0]
+        cur_sum = 0
+        for i in range(len(nums)):
+            cur_sum = max(nums[i], cur_sum+nums[i])
+            max_sum = max(max_sum, cur_sum)
+        return max_sum
